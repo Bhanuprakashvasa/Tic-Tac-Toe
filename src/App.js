bfdf57a023ac935888117bbbecd296f24abfdf2a
mr_bhanu__33 <115440020+Bhanuprakashvasa@users.noreply.github.com>
2023-03-03 10:27:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+
+
+      
+  function Game(){
+    const [arr,setArr]=useState([null,null,null,null,null,null,null,null,null,])
+    const [isX,setIsX] = useState(true);
+
+    const click=(index)=>
+{
+      if(result==null && arr[index]==null){
+      const copy=[...arr];
+      copy[index]=isX ? "X" : "0";
+      setArr(copy);
+
+      setIsX(!isX);
+     
+    };
+    
+}
+
+
+const winner=(arr)=>{
+  const lines=[
+    [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],
+  ];
+  for (let i=0;i<lines.length;i++)
+  {
+    const[a,b,c]=lines[i];
+    if(arr[a] !=null && arr[a]==arr[b] && arr[b]==arr[c])
+    {
+    console.log("Winner winner Chicken Dinner",arr[a])
+    return arr[a];
+    }
+  }
+  return null;
+  
+};
+const restart=() =>{
+
+  setArr([null,null,null,null,null,null,null,null,null,]);
+  setIsX(true);
+
+};
+   const result= winner(arr);
+   const { width, height } = useWindowSize()
+
+    return (
+        <div className='bp'>
+          {result ? (
+          <Confetti width={width} height={height} />): null}
+  
+          <h1>Tic Tac Toe Game</h1>
+          <div className='arr'> {arr.map((val,index)=>(
+          <Box val={val} playerClick={()=>click(index)} />))}
+          </div>
+          {result ? <h1>winner is :{result}</h1> : null}
+          <div className='vbp'>
+          <button onClick={restart} >Reload</button>
+          </div>
+
+        
+         
+        </div>
+      );
+}
+
+function Box({val,playerClick}){
+  // const [val,setVal]=useState("X");
+  const style= {
+    color:val == "X" ? "Violet" : "Yellow",
+  };
+ return (
+  <div style={style} onClick={playerClick} className='box'>
+    {val}
+  </div>
+ );
+}
+
+
+
+
+export default App;
